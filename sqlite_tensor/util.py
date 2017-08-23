b02094fe025dca9ff64969a3141e87cba3bb82e1
osoken <osoken.devel@outlook.jp>
2017-08-24 00:28:04 +0900

    remove ts2dt

--- sqlite_tensor/util.py
+++ sqlite_tensor/util.py
@@ -22,11 +22,5 @@ def dt2ts(dt):
     return int(time.mktime(dt.timetuple()) * 1000) + (dt.microsecond // 1000)
 
 
-def ts2dt(ts):
-    return datetime.fromtimestamp(
-        int(ts) // 1000
-    ).replace(microsecond=(int(ts) % 1000 * 1000))
-
-
 def now():
     return dt2ts(datetime.now())
